f8d58f1edc93e6cf06ef22ad70c0c331ad11a75d
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-14 11:00:20 +0800

    Deploy goerli

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -11,13 +11,13 @@ async function main() {
 
   // Fill with correct data and uncomment the correct network before deploy!
   // const teamWallet = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc"; // localhost
-  const teamWallet = "0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd"; // goerli
+  const teamWallet = "0x6779249234bdA39Cb7AF2aA4e35d658d7548DDAe"; // goerli
   // const teamWallet = ""; // mainnet
   // const teamWallet = "0xCaC8c3f44f913b012D304d36E94BA124B1Ca8A9B"; // mumbai
   
   // Fill with correct data and uncomment the correct network before deploy!
   // const whitelistAddresses = [teamWallet, "0x70997970c51812dc3a010c7d01b50e0d17dc79c8"] // localhost
-  const whitelistAddresses = [teamWallet] // goerli
+  const whitelistAddresses = [teamWallet, "0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd"] // goerli
   // const whitelistAddresses = [teamWallet] // mainnet
   // const whitelistAddresses = [teamWallet] // mumbai
   
